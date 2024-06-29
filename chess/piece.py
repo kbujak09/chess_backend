a341bc2f4b35650ac6db32597fb6b74ae22a4cf7
Kacper Bujak <kaccpper2@gmail.com>
2024-06-29 16:21:26 +0200

    all figures functions that returns possible moves and tests

--- chess/piece.py
+++ chess/piece.py
@@ -3,6 +3,30 @@ class Piece:
     self.piece_type = piece_type
     self.color = color
     
+class LineMoverPiece(Piece):
+  def moves_in_directions(self, position, board, move_directions):
+    moves = []
+    x, y = position
+  
+    for d in move_directions:
+      row, col = x, y
+      while True:
+        row += d[0]
+        col += d[1]
+        if 0 <= row < 8 and 0 <= col < 8:
+          if board[row][col] is None:
+            moves.append((row, col))
+          elif board[row][col].color != self.color:
+            moves.append((row, col))
+            break 
+          else:
+            break     
+        else:
+          break
+    return moves
+
+    
+    
 class Pawn(Piece):
   def __init__(self, color):
     super().__init__('pawn', color)
@@ -23,46 +47,71 @@ class Pawn(Piece):
       if (8 > y + 1) and board[x + direction][y + 1] is not None and board[x + direction][y + 1].color != self.color:
         moves.append((x + direction, y + 1))
     return moves
-  
 pass
     
-class Bishop(Piece):
+class Bishop(LineMoverPiece):
   def __init__(self, color):
     super().__init__('bishop', color)
     
   def moves(self, position, board):
-    moves = []
-    x, y = position
-    direction = 1 if self.color == 'white' else -1
     move_directions = [(1, 1), (-1, 1), (1, -1), (-1, -1)]
-  
-    for d in move_directions:
-      row, col = x, y
-      while True:
-        row += d[0]
-        col += d[1]
-        if 0 <= row < 8 and 0 <= col < 8 and board[row][col] is None:
-          if (row, col) not in moves:
-            moves.append((row, col))      
-        else:
-          break
-    print(moves)
-    return moves
+    return self.moves_in_directions(position, board, move_directions)
 pass
       
     
 class Knight(Piece):
   def __init__(self, color):
     super().__init__('knight', color)
+  
+  def moves(self, position, board):
+    moves = []
+    x, y = position
+    move_direction = [(2, 1), (2, -1), (1, 2), (1, -2), (-2, 1), (-2, -1), (-1, 2), (-1, -2)]
     
-class Rook(Piece):
+    for d in move_direction:
+      temp_x = x + d[0]
+      temp_y = y + d[1]
+      if 0 <= temp_x < 8 and 0 <= temp_y < 8 and board[temp_x][temp_y]is None:
+          moves.append((temp_x, temp_y))
+      elif board[temp_x][temp_y]is not None and board[temp_x][temp_y].color != self.color:
+          moves.append((temp_x, temp_y))
+    return moves
+pass
+    
+class Rook(LineMoverPiece):
   def __init__(self, color):
     super().__init__('rook', color)
     
-class Queen(Piece):
+  def moves(self, position, board):
+    move_directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    return self.moves_in_directions(position, board, move_directions)
+pass
+    
+    
+class Queen(LineMoverPiece):
   def __init__(self, color):
     super().__init__('queen', color)
-    
+  
+  def moves(self, position, board):
+    move_directions = [(1, 1), (-1, 1), (1, -1), (-1, -1), (0, 1), (0, -1), (1, 0), (-1, 0)]
+    return self.moves_in_directions(position, board, move_directions)
+pass
+
 class King(Piece):
   def __init__(self, color):
     super().__init__('king', color)
+    
+  def moves(self, position, board):
+    moves = []
+    x, y = position
+    move_directions = [(1, -1), (1, 0), (1, 1),
+                       (0, -1), (0, 1),
+                       (-1, -1), (-1, 0), (-1, 1)]
+    
+    for d in move_directions:
+      row = x + d[0]
+      col = y + d[1]
+      if board[row][col] is None or board[row][col].color != self.color:
+        moves.append((row, col))
+    return moves
+pass
--- test_chess.py
+++ test_chess.py
@@ -4,43 +4,50 @@ from chess.board import Board
 
 from chess.piece import Pawn, Bishop, Knight, Rook, Queen, King
 
-empty_board = [[None for _ in range(8)] for _ in range(8)]
-
-test_board = Board()
+@pytest.fixture(autouse=True)
+def setup():
+    empty_board = [[None for _ in range(8)] for _ in range(8)]
+    test_board = Board()
+    return empty_board, test_board
 
 def test_pawn_init():
-  white_pawn = Pawn('white')
-  black_pawn = Pawn('black')
+    white_pawn = Pawn('white')
+    black_pawn = Pawn('black')
+    
+    assert white_pawn.piece_type == 'pawn'
+    assert white_pawn.color == 'white'
+    
+    assert black_pawn.piece_type == 'pawn'
+    assert black_pawn.color == 'black'
   
-  assert white_pawn.piece_type == 'pawn'
-  assert white_pawn.color == 'white'
-  
-  assert black_pawn.piece_type == 'pawn'
-  assert black_pawn.color == 'black'
-  
-def test_white_pawn_moves():
+def test_white_pawn_moves(setup):
+    empty_board, _ = setup
     white_pawn = Pawn('white')
     empty_board[3][1] = white_pawn
     assert white_pawn.moves((3, 1), empty_board) == [(4, 1)]
-    
-def test_black_pawn_moves():
+  
+def test_black_pawn_moves(setup):
+    empty_board, _ = setup
     black_pawn = Pawn('black')
     empty_board[5][0] = black_pawn
     assert black_pawn.moves((5, 0), empty_board) == [(4, 0)]
     
-def test_double_pawn_move():
+def test_double_pawn_move(setup):
+    empty_board, _ = setup
     white_pawn = Pawn('white')
     empty_board[1][0] = white_pawn
-    assert white_pawn.moves((1, 0), empty_board) == [(2,0), (3, 0)]   
+    assert white_pawn.moves((1, 0), empty_board) == [(2, 0), (3, 0)]
     
-def test_blocked_pawn_moves():
+def test_blocked_pawn_moves(setup):
+    empty_board, _ = setup
     white_pawn = Pawn('white')
     black_pawn = Pawn('black')
     empty_board[1][0] = white_pawn
     empty_board[2][0] = black_pawn
     assert white_pawn.moves((1, 0), empty_board) == []
     
-def test_figures_placement():
+def test_figures_placement(setup):
+    _, test_board = setup
     figures = ['rook', 'knight', 'bishop', 'queen', 'king', 'bishop', 'knight', 'rook']
     for i in range(8):
         assert test_board.board[0][i].piece_type == figures[i] and test_board.board[0][i].color == 'white'
@@ -48,10 +55,58 @@ def test_figures_placement():
         assert test_board.board[7][i].piece_type == figures[i] and test_board.board[7][i].color == 'black'
         assert test_board.board[6][i].piece_type == 'pawn' and test_board.board[6][i].color == 'black'
 
-def test_bishop_moves():
+def test_bishop_moves(setup):
+    empty_board, _ = setup
     bishop = Bishop('white')
+    pawn = Pawn('white')
     empty_board[4][5] = bishop
-    assert sorted(bishop.moves((4, 5), empty_board)) == sorted([(5, 4), (6, 3), (7, 2), (5, 6), (6, 7), (3, 4), (2, 3), (1, 2), (0, 1), (3, 6), (2, 7)])
-        
+    empty_board[5][4] = pawn
+    print(sorted(bishop.moves((4, 5), empty_board))) 
+    assert sorted(bishop.moves((4, 5), empty_board)) == sorted([(5, 6), (6, 7), (3, 4), (2, 3), (1, 2), (0, 1), (3, 6), (2, 7)])
     
+def test_blocked_bishop_moves(setup):
+    empty_board, _ = setup
+    bishop = Bishop('black')
+    pawn = Pawn('black')
+    empty_board[0][0] = bishop
+    empty_board[2][2] = pawn
+    print(bishop.moves((0, 0), empty_board))
+    assert bishop.moves((0, 0), empty_board) == [(1, 1)]
     
+def test_knight_moves(setup):
+    empty_board, _ = setup
+    knight = Knight('white')
+    pawn = Pawn('white')
+    empty_board[4][5] = knight
+    empty_board[5][3] = pawn
+    assert sorted(knight.moves((4, 5), empty_board)) == sorted([(6, 4), (2, 4), (3, 3), (6, 6), (5, 7), (3, 7), (2, 6)]) 
+    
+def test_rook_moves(setup):
+    empty_board, _ = setup
+    rook = Rook('white')
+    black_pawn = Pawn('black')
+    white_pawn = Pawn('white')
+    empty_board[0][0] = rook
+    empty_board[4][0] = white_pawn
+    empty_board[0][4] = black_pawn
+    assert sorted(rook.moves((0, 0), empty_board)) == sorted([(0, 1), (0, 2), (0, 3), (0, 4), (1, 0), (2, 0), (3, 0)])
+    
+def test_queen_moves(setup):
+    empty_board, _ = setup
+    queen = Queen('white')
+    white_pawn = Pawn('white')
+    black_pawn = Pawn('black')
+    empty_board[2][3] = queen
+    empty_board[3][3] = black_pawn
+    empty_board[2][5] = white_pawn
+    assert sorted(queen.moves((2,3), empty_board)) == sorted([(5, 0), (4, 1), (3, 2), (2, 0), (2, 1), (2, 2), (0, 3), (1, 3), (3, 4), (4, 5), (5, 6), (6, 7), (2, 4), (3, 3), (1, 4), (0, 5), (0, 1), (1, 2)])
+
+def test_king_moves(setup):
+    empty_board, _ = setup
+    king = King('white')
+    white_pawn = Pawn('white')
+    black_pawn = Pawn('black')
+    empty_board[3][3] = king
+    empty_board[4][3] = white_pawn
+    empty_board[3][4] = black_pawn
+    assert sorted(king.moves((3,3), empty_board)) == sorted([(4, 2), (4, 4), (3, 2), (3, 4), (2, 2), (2, 3), (2, 4)])
