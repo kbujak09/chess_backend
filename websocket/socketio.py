46fc153c892ab22ddd5e609aa9faa1dee0ea5095
Kacper Bujak <kaccpper2@gmail.com>
2024-09-17 18:58:18 +0200

    remove rooms dictionary logic

--- websocket/socketio.py
+++ websocket/socketio.py
@@ -1,20 +1,13 @@
 from flask_socketio import join_room, leave_room, send, emit
 from . import socketio
 
-rooms = {}
-
 @socketio.on('join')
 def on_join(data):
   user_id = data['id']
   username = data['username']
   room = data['room']
-  
-  print(user_id, username)
-  
+    
   join_room(room)
-  
-  if room not in rooms:
-    rooms[room] = []
   
   send(user_id + ' has entered the room.', to=room)
   
